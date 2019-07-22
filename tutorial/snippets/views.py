5b18d6faec4a6eaa48658ca06f7769eb7733780e
JunGu Kang <chr0m3.kr@gmail.com>
2019-07-22 17:38:00 +0900

    Tutorial 6

--- tutorial/snippets/views.py
+++ tutorial/snippets/views.py
@@ -3,10 +3,10 @@ from rest_framework import (
     generics,
     permissions,
     renderers,
+    viewsets,
 )
-from rest_framework.decorators import api_view
+from rest_framework.decorators import action
 from rest_framework.response import Response
-from rest_framework.reverse import reverse
 
 from snippets.models import Snippet
 from snippets.permissions import IsOwnerOrReadOnly
@@ -16,12 +16,22 @@ from snippets.serializers import (
 )
 
 
-@api_view(('GET', ))
-def api_root(request, format=None):
-    return Response({
-        'users': reverse('user-list', request=request, format=format),
-        'snippets': reverse('snippet-list', request=request, format=format),
-    })
+class SnippetViewSet(viewsets.ModelViewSet):
+    """
+    This viewset automatically provides 'list', 'create', 'retrieve', 'update' and 'destroy' actions.
+    Additionally we also provide an extra 'highlight' action.
+    """
+    queryset = Snippet.objects.all()
+    serializer_class = SnippetSerializer
+    permission_classes = (permissions.IsAuthenticatedOrReadOnly, IsOwnerOrReadOnly)
+
+    @action(detail=True, renderer_classes=(renderers.StaticHTMLRenderer, ))
+    def highlight(self, request, *args, **kwargs):
+        snippet = self.get_object()
+        return Response(snippet.highlighted)
+
+    def perform_create(self, serializer):
+        serializer.save(owner=self.request.user)
 
 
 class SnippetList(generics.ListCreateAPIView):
@@ -48,11 +58,9 @@ class SnippetHighlight(generics.GenericAPIView):
         return Response(snippet.highlighted)
 
 
-class UserList(generics.ListAPIView):
-    queryset = User.objects.all()
-    serializer_class = UserSerializer
-
-
-class UserDetail(generics.RetrieveAPIView):
+class UserViewSet(viewsets.ReadOnlyModelViewSet):
+    """
+    This viewset automatically provides 'list' and 'detail' actions.
+    """
     queryset = User.objects.all()
     serializer_class = UserSerializer
